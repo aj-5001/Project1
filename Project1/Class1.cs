897adfc2ce27a1cfd573452e8087eaf0a9fbede2
aj-5001 <alecjasanovsky@gmail.com>
2018-04-19 15:50:48 -0400

    Commit 2

--- Project1/Class1.cs
+++ Project1/Class1.cs
@@ -15,19 +15,37 @@ namespace HelloWorld
             Console.WriteLine("Press the any key to continue.");
             String a = Console.ReadLine();
 
-            if a == "Any key" {
+            if (a == "Any key") {
                 Console.WriteLine("Wow, you hit the any key!, now type anything to leave.");
+                Console.WriteLine("Would you like to start your adventure now? Y/N")
+               
                 Console.ReadKey();
             }
             else
             {
                 Console.WriteLine("You failed!");
+                Console.WriteLine("I'll give you another try, do you wish to begin your adventure? Y/N:");
+                String answer = Console.ReadLine();
+                if (answer == "Y" || answer == "y")
+                {
+                    Console.WriteLine("Great job! It's time to start your trek to the castle!");
+                    Console.WriteLine("Press any key to begin!");
+                    if (Console.KeyAvailable == false)
+                    {
+                        castleTrek();
+                    }
+
+                }
                 Console.ReadKey();
             }
       
             
-
-            
         }
+
+        static public void castleTrek()
+        {
+            Console.WriteLine("You begin walking.. and you walk for some time.. then you walk some more");
+        }
+        
     }
 }
