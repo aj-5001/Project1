279a8a47f6ca8c7b78c758311b9e3c5ffdd33df3
aj-5001 <alecjasanovsky@gmail.com>
2018-04-19 11:49:58 -0400

    commit 1
    while closed

---
+++ Project1/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main()
+        {
+            Console.WriteLine("The adventure world:");
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press the any key to continue.");
+            String a = Console.ReadLine();
+
+            if a == "Any key" {
+                Console.WriteLine("Wow, you hit the any key!, now type anything to leave.");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("You failed!");
+                Console.ReadKey();
+            }
+      
+            
+
+            
+        }
+    }
+}
