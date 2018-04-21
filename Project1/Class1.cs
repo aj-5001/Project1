e96a4007797ec9ee42e5a6d047b933b7f70ae807
aj-5001 <alecjasanovsky@gmail.com>
2018-04-21 02:14:39 -0400

    beginning timer work
    updated system references and started working on implementing timer

--- Project1/Class1.cs
+++ Project1/Class1.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
+using System.Timers;
+
 namespace HelloWorld
 {
     class Hello
     {
+        static private int timerCount = 0;
+       
+
+        
+        
         static void Main()
         {
+            
             Console.WriteLine("The adventure world:");
-
+            
             // Keep the console window open in debug mode.
             Console.WriteLine("Press the any key to continue.");
             String a = Console.ReadLine();
@@ -56,7 +66,9 @@ namespace HelloWorld
         {
             //journey to the castle
             Console.WriteLine("You begin walking.. and you walk for some time.. then you walk some more");
+            hitSpaceToContinue();
             Console.WriteLine("Suddenly, you come across three bandits attacking a carriage! Will you attack, or hide and watch?");
+            hitSpaceToContinue();
             Console.WriteLine("Press 1 to attack | Press 2 to watch");
             bool checkAnswer = false;
             string answer = null;
@@ -65,24 +77,63 @@ namespace HelloWorld
             {
                 if (answer == "1")
                 {
-                    //do something
-                    Console.WriteLine("test complete");
+                    //Begins the attack sequence 
+                    Console.WriteLine("You jump out of the bushes, and the first one charges!");
+                    hitSpaceToContinue();
+                    Console.WriteLine("You have 5 seconds to answer!");
+                    Console.WriteLine("Press 1 - Slash!");
+                    Console.WriteLine("Press 2 - Dogde!");
+                    Timer timer = new Timer(Hello.timerCallback, null, 1000, Timeout.Infinite);
+                    
+                    
+
+                    
                     checkAnswer = true;
                 }
                 else if (answer == "2")
                 {
-                    // do something else
-                    Console.WriteLine("test compelete");
+                    // Watch her get slaughtered and restart the game
+                    Console.WriteLine("test compelete - still need to restart the game");
                     checkAnswer = true;
                 }
                 else if (answer != "1" || answer != "2")
                 {
-                    Console.WriteLine("Give a proper answer, 1 or 2: ");
-                    Console.Write("Current Answer: ");
-                    Console.Write(answer);
+                    Console.Write("Give a proper answer, 1 or 2: ");
                     answer = Console.ReadLine();
                 }
             }
+            
+        }
+
+        static public void hitSpaceToContinue()
+        {
+            // function to ease hitting space to continue
+            Console.WriteLine("Hit space to continue..");
+            bool checkanswer = false;
+            string answer = Console.ReadLine();
+            while (checkanswer == false)
+            {
+                if (answer == " ")
+                {
+                    checkanswer = true;
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Hit the spacebar..");
+                    answer = Console.ReadLine();
+                }
+            }
+
+        }
+
+        static public void timerCallback(Object _)
+        {
+            if (timerCount > 5)
+            {
+                
+            }
+            timerCount++;
             
         }
         
